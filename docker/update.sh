871496c12b8b89179bf63fff2f65d7c55f598702
thomasthethermonuclearbomb <morrissebestyen@gmail.com>
2021-02-14 00:14:29 +0100

    docker 11
    testing out healthcheck instead of deploy.sh

--- docker/update.sh
+++ docker/update.sh
@@ -1,15 +1,15 @@
-while [ 1 ];
-do
-    git pull
-
-    if [ "$?" != 0 ]
-    then
-        echo "error pulling new changes\n"
-        killall python3
-        shutdown now
-        exit
-    fi
-
-    sleep 60
-done
-
+#while [ 1 ];
+#do
+#    git pull
+#
+#    if [ "$?" != 0 ]
+#    then
+#        echo "error pulling new changes\n"
+#        killall python3
+#        shutdown now
+#        exit
+#    fi
+#
+#    sleep 60
+#done
+#
