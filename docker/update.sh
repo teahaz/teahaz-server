70c92479f9e88f58c8a24fdc36bb0a9dda79673a
thomasthethermonuclearbomb <morrissebestyen@gmail.com>
2021-02-14 01:47:15 +0100

    docker 15
    cleaning up update.sh

--- docker/update.sh
+++ docker/update.sh
@@ -4,10 +4,9 @@ do
 
     if [ "$?" != 0 ]
     then
+       echo "\e[0;31m==========================="
        echo "error pulling new changes\n"
-        killall python3
-        shutdown now
-        exit
+       echo "===========================\e[0;37m"
     fi
 
     sleep 60
