c83716f0808ade4f2a0c9fa86abb0b3c38ecffa4
thomasthethermonuclearbomb <morrissbestyen@gmail.com>
2021-02-15 09:34:25 +0100

    docker 19
    * update now sends logs with the python logging function as well

--- docker/update.sh
+++ docker/update.sh
@@ -4,9 +4,13 @@ do
 
     if [ "$?" != 0 ]
     then
+        # pull failed
        echo "\e[0;31m==========================="
        echo "error pulling new changes"
        echo "===========================\e[0;37m"
+
+        # oneliner uses the python logging function to create a log of git failing
+        cd ../server/; python -c "import logging_th as log;log.logger(level='warning', msg='failed to pull updates from git')";cd ../docker
     fi
 
     sleep 60
